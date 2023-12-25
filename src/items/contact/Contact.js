15601c55de1d9c73c6b50157833405a9b9b24031
ADITYA SINGH SIKARWAR <bs151439@gmail.com>
2023-12-25 13:14:35 +0530

    commit

--- src/items/contact/Contact.js
+++ src/items/contact/Contact.js
@@ -3,6 +3,11 @@ import './Contact.css'
 import instagram from "../../images/instagram.png"
 import linkedin from "../../images/linkedin2.png"
 import github from "../../images/github2.png"
+import { FaInstagram } from "react-icons/fa";
+import { SiLinkedin } from "react-icons/si";
+import { FaGithub } from "react-icons/fa";
+
+
 
 const Contact = () => {
 
@@ -14,13 +19,14 @@ const Contact = () => {
 
 <div id='Contact' className="footer">
 <div className="row1">
-<a href="https://www.instagram.com/aadityeahh/"><img src={instagram} alt="" /></a>
-<a href="https://www.linkedin.com/in/adityasikarwar1/"><img src={linkedin} alt="" /></a>
-<a href="https://github.com/Adityasikarwar70"><img src={github} alt="" /></a>
+<a href="https://www.instagram.com/aadityeahh/"><FaInstagram /></a>
+<a href="https://www.linkedin.com/in/adityasikarwar1/"><SiLinkedin/></a>
+<a href="https://github.com/Adityasikarwar70"><FaGithub/></a>
 </div>
 
 <div className="row">
 <ul>
+  
 <li><a className="nav-link active" href="#First">Home</a></li>
 <li><a className="nav-link active" href="#Second">About us</a></li>
 <li><a className="nav-link active" href="#Skills">Skills </a></li>
